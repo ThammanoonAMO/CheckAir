6eea1ff6ce9295f89d5db53b751296e1ce28d3d3
thammanoon <ten_15370@hotmail.com>
2019-05-22 03:03:08 +0700

    “message”

--- src/page/home/selectLocation.js
+++ src/page/home/selectLocation.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, Button, StyleSheet, FlatList } from "react-native";
 
 export default class Location extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    return fetch("http://localhost:5000/api/data/data")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson
+          },
+          function() {}
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
   render() {
     return (
       <View style={styles.MainContainer}>
         <Text style={{ fontSize: 23 }}> Screen 2 </Text>
-        <Button
-          title="Open Hidden Screen in drawer"
-          onPress={() => this.props.navigation.navigate("ShowAQI")}
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <Button
+              title={item.name}
+              onPress={() => {
+                this.props.navigation.navigate("ShowAQI", {
+                  name: item.name,
+                  AQI: item.AQI,
+                  subject: item.subject
+                });
+              }}
+            />
+          )}
+          keyExtractor={({ id }, index) => id}
         />
       </View>
     );
--- src/page/home/showAQI.js
+++ src/page/home/showAQI.js
@@ -2,42 +2,47 @@ import React, { Component } from "react";
 import { Text, View, StyleSheet, Button, FlatList } from "react-native";
 
 export default class ShowAQI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true };
-  }
-
-  componentDidMount() {
-    return fetch("http://localhost:5000/api/data/data")
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState(
-          {
-            isLoading: false,
-            dataSource: responseJson
-          },
-          function() {}
-        );
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
   render() {
+    const { navigation } = this.props;
+    var name = navigation.dangerouslyGetParent().getParam("name", "NO-NAME");
+    var AQI = navigation.dangerouslyGetParent().getParam("AQI", "NO-AQI");
+    var subject = navigation
+      .dangerouslyGetParent()
+      .getParam("subject", "NO-subject");
+
     return (
-      <View style={StyleSheet.MainContrainer}>
-        <Text style={{ fontSize: 23 }}>Screen Show work!!!</Text>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({ item }) => <Text>{item.AQI}</Text>}
-          keyExtractor={({ id }, index) => id}
-        />
+      <View style={styles.MainContrainer}>
+        <Text style={styles.District}>{JSON.stringify(name)}</Text>
+        <Text style={styles.Name}>{JSON.stringify(subject)}</Text>
+        <Text>{JSON.stringify(AQI)}</Text>
         <Button
           title="Back to screen2"
           onPress={() => this.props.navigation.navigate("Location")}
+        />
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            this.props.navigation.push("ShowAQI", {
+              name: "test"
+            })
+          }
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  MainContrainer: {
+    flex: 1,
+    paddingTop: 5,
+    alignItems: "center",
+    marginTop: 5
+  },
+  District: {
+    fontSize: 30
+  },
+  Name: {
+    fontSize: 20
+  }
+});
